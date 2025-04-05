2ef7015e65175a54e9f31b322fd57ac165e94189
Praneeth22 <yennampraneeth@gmail.com>
2025-04-05 16:28:59 -0400

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules:{
+      "react/react-in-jsx-scope": "off", // Not needed with Next.js
+     
+    }
+  }
 ];
 
 export default eslintConfig;
